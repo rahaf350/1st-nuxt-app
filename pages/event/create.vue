4ea7d6af9370c138ae4ae07d95f4b297c7411eeb
rahaf <rahafalm1@gmail.com>
2023-09-09 21:36:29 +0300

    add nuxt project

---
+++ pages/event/create.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <h1>Create An Event</h1>
+    </div>
+</template>
+
+<script>
+export default{
+  head() {
+    return {
+        title: 'create',
+        meta: [
+            {
+                hid: 'description',
+                name: 'description',
+                content: 'hello there'
+            }
+        ]
+    }
+  }
+}
+</script>
